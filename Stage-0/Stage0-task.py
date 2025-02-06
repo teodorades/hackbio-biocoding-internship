ed440c18548ab6de00152898236950b62fdf48d2
teodorades <teodoradespotovski@gmail.com>
2025-02-06 18:42:40 +0100

    New file

---
+++ Stage-0/Stage0-task.py
@@ -0,0 +1,44 @@
+""" dictionary 
+    with inforamtion 
+    of each team member 
+"""
+info = {
+    "Names": "Teodora, Alvin, Bilal, Tanson, Natt",
+    "Slack usernames" : "Teodora, Alvin, bilal ashraf, K.Tanson, Natt",
+    "Emails" : "teodoradespotovski@gmail.com, alvinobande123@gmail.com, eclecticbilalashraf@gmail.com, Kelvins.tanson@gmail.com, im.nathn@gmail.com",
+    "Hobbies" : "Cooking, Music, Mixed Martial Arts, No info, Read manga",
+    "Countries" : "Serbia, Nigerian, Germany, No info, Australia",
+    "Disciplines" : "Biochemistry, Medical Biotechnology, Bioinformatics, No info, Biotechnology",
+    "Preferred programming language" : "Python, Python, Python, R, Python"
+} 
+
+""" Creating variables for each value in dictionary. The .split  was used divide the string in diffine place (", ")
+(important if we want to print one member of the group): """
+Names = info["Names"].split(", ")
+Usernames = info["Slack usernames"].split(", ")
+Emails = info["Emails"].split(", ")
+Hobbies = info["Hobbies"].split(", ")
+Countries = info["Countries"].split(", ")
+Disciplines = info["Disciplines"].split(", ")
+Preffered_programming_language = info["Preferred programming language"].split(", ")
+
+# Print all team members information, using .join to return the one string (beacuse .split was used above):
+print(f"""Team members information: 
+ Names: {(", ").join(Names)} 
+ Usernames: {(", ").join(Usernames)} 
+ Emails: {(", ").join(Emails)} 
+ Hobbies: {(", ").join(Hobbies)} 
+ Countries: {(", ").join(Countries)} 
+ Disciplines: {(", ").join(Disciplines)} 
+ Preffered programming langugae: {(", ").join(Preffered_programming_language)}""")
+
+# Print information for only one team member, that is why the .split was used before:
+print(f"""\nInformation for one team member: 
+ Name: {Names[0]}
+ Username: {Usernames[0]}
+ Email: {Emails[0]} 
+ Hobby: {Hobbies[0]}
+ Country: {Countries[0]}
+ Discipline: {Disciplines[0]} 
+ Preffered_programming_language: {Preffered_programming_language[0]}""")
+ # Note: If you want to print information for all other group memembrs you can just simply change [0] to [1]-[4]
