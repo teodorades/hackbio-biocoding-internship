6cee203af71abf2ce498ad29994f6db6a21c3783
teodorades <teodoradespotovski@gmail.com>
2025-02-12 12:49:22 +0100

    Added fourth function

--- Stage-1/Stage-1.py
+++ Stage-1/Stage-1.py
@@ -27,4 +27,47 @@ def translate(DNA):
 DNA = 'ATGGCCCTGTGGATGCGGGGAAGGCCAGGCACCGAGGGCCAGGGACCTGGCGGCTCTTCTACCAGCAGCAGCCTGTGGATGCGGGGAAGGCCAGGCACCGAGGGCCAGGGACCTGGCGGCTCTTCTACCAGCAGCAGCCTGTGGATGCGGGGAAGGCCAGGCACCGAGGGCCAGGGACCTGGCGGCTCTTCTACCAGCAGCAGCTGCCCTGGCAGCTGGAGGCGGGGCAGCCTGGAGCAGGACCTGCAGATCGCGGGCATCGCGGGCGGAGGAGGAGGGTGCCGCGCGGGAGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGAGGCGGCGGA'
 print(translate(DNA))
 
+# Forth function
+""" Finally, write a function for calculating the hamming distance
+ between your Slack username and twitter/X handle (synthesize if you don’t have one). 
+ Feel free to pad it with extra words if they are not of the same length."""
+
+
+def compare(x_username, slack_username):
+    hamming_distance = 0
+    for i in range(len(x_username)):
+        if x_username[i] != slack_username[i]:
+            hamming_distance += 1
+    return hamming_distance
+print(compare('desteodora', 'Teodoraaaa'))
+
+# Second function
+""" Write a function that simulates and generates a logistic population growth curve. 
+Your function should include 2 extra parameters that randomize the length of the lag phase and the exponential phase [See population curve here].
+Most living populations follow a logistic population growth. 
+Therefore, your growth curve can be: Population Size vs Time, Cell density vs Time, OD vs Time, CFU vs Time, etc
+Using your function, generate a dataframe with 100 different growth curves """
+import math
+import random
+import pandas as pd
+
+def logistic_growth_cruve(initial_population, time, max_value, lag_phase, expo_phase):
+    for time in range(time - lag_phase, 400):
+        population_size = max_value/ (1+ ((max_value - initial_population)/initial_population) * math.exp(-expo_phase * (time - lag_phase)))
+        # defined stationary and dead phase
+        if 20 <= time <= 150:
+          population_size = 20
+        elif time <=150:
+         population_size -= 50
+
+def generate_logistic_gowth_curve(num = 100):
+    rows  = [] 
+
+    for i in range(num):
+        initial_population = random.randint(0,5)
+        max_value = random.randint(50, 200)
+        lag_phase = random.randint(0,10)
+        expo_phase = random.randint(0.1,1)
+         
+print(logistic_growth_cruve())
 
