340c530184fd8914da4d99f810875145b67bf071
teodorades <teodoradespotovski@gmail.com>
2025-02-21 10:27:13 +0100

    test.py updated

--- Stage-2/test.py
+++ Stage-2/test.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 sift_data = 'https://raw.githubusercontent.com/HackBio-Internship/public_datasets/main/R/datasets/sift.tsv'
 fold_data = 'https://raw.githubusercontent.com/HackBio-Internship/public_datasets/main/R/datasets/foldX.tsv'
@@ -10,25 +12,49 @@ fold_df = pd.read_csv(fold_data, sep=r'\s+')
 
 sift_df["specific_Protein_aa"] = sift_df["Protein"] + '_' + sift_df["Amino_Acid"]
 fold_df["specific_Protein_aa"] = fold_df["Protein"] + '_' + fold_df["Amino_Acid"]
-modified_sift_df = sift_df.drop(["Protein", "Amino_Acid"], axis='columns')
-modified_fold_df = fold_df.drop(["Protein", "Amino_Acid"], axis='columns')
-
-# print(modified_sift_df, '\n', modified_fold_df) # --> check if the columns were added correctly
+# print(sift_df, '\n', fold_df) # --> check if the columns were added correctly
 
 # merge datasets with specific_Protein_aa
-merged_df = pd.merge(modified_fold_df, modified_sift_df, on="specific_Protein_aa")
+merged_df = pd.merge(fold_df, sift_df, on="specific_Protein_aa")
+
+# Drop duplicate columns
+merged_df = merged_df.drop(columns=["Protein_y", "Amino_Acid_y"])
+
+# Rename remaining columns for clarity
+merged_df = merged_df.rename(columns={"Protein_x": "Protein", "Amino_Acid_x": "Amino_Acid"})
+
 # print(merged_df) # --> check
 
 # A SIFT Score below 0.05 is deleterious
 delet_sift = merged_df[merged_df["sift_Score"] < 0.05]
 # A FoldX score greater than 2 kCal/mol is deleterious
 delet_fold = merged_df[merged_df["foldX_Score"] > 2]
-print(delet_fold, delet_sift)
+# print(delet_fold, delet_sift) --> check
 
 mutants = []
-for i in delet_sift['specific_Protein_aa']:  
-    if i in delet_fold['specific_Protein_aa'].values:  
+for i in delet_sift["specific_Protein_aa"]:  
+    if i in delet_fold["specific_Protein_aa"].values:  
         mutants.append(i) 
+# print(mutants)
+# print(len(mutants)) # --> check
 
-print(mutants)
-print(len(mutants))
+""" Study the amino_acid_substitution_nomenclature
+(https://atlasgeneticsoncology.org/teaching/30067/nomenclature-for-the-description-of-mutations-and-other-sequence-variations#section-3)"""
+
+# Extract the original (wild-type) amino acid
+merged_df["Wild_Type_AA"] = merged_df["Amino_Acid"].str[0]
+# print(merged_df) # --> check
+
+# Generate frequency table:
+freq_aa = merged_df["Wild_Type_AA"].value_counts()
+print(freq_aa)
+
+# Generate a barplot:
+plt.bar(x=freq_aa.index, height=freq_aa.values)
+plt.xlabel("Wild Type AA")
+plt.ylabel("Frequency")
+plt.show()
+
+# Generate a pie-chart:
+plt.pie(freq_aa.values, labels=freq_aa.index)
+plt.show()
