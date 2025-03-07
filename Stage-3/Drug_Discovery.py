efeb661bee6b2a3867e3ad565483976975a7d810
teodorades <teodoradespotovski@gmail.com>
2025-03-07 17:05:29 +0100

    changed

--- Stage-3/Drug_Discovery.py
+++ Stage-3/Drug_Discovery.py
@@ -3,71 +3,89 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 import numpy as np
-from sklearn.metrics import silhouette_samples, silhouette_score
 import matplotlib.patches as mpatches
 from matplotlib.colors import Normalize
  
-# Import data and create dataframe
-data = "https://raw.githubusercontent.com/HackBio-Internship/2025_project_collection/refs/heads/main/Python/Dataset/drug_class_struct.txt"
-df = pd.read_csv(data, sep="\t")
+# Create dataframe form imported data
+data_url = "https://raw.githubusercontent.com/HackBio-Internship/2025_project_collection/refs/heads/main/Python/Dataset/drug_class_struct.txt"
+df = pd.read_csv(data_url, sep="\t")
  
-# Select only numeric columns for PCA analysis that has only chemical properties
-numeric_columns = df.drop(columns=['ID', 'SMILES', 'target'])
+# Selected features
+features = df.drop(columns=['ID', 'SMILES', 'target', 'score'])
  
-# Scale the data - before applying the clustering algoritham, ensuring that each feture contribute equally
+# Scale the data - ensuring that each feture contribute equally
 scaler = StandardScaler()
-scaled_data = scaler.fit_transform(numeric_columns)
+scaled_features = scaler.fit_transform(features)
  
 # PCA
-pca = PCA(n_components=2) # 2D
-principal_components = pca.fit_transform(scaled_data)
-print(np.sum(pca.explained_variance_ratio_)) # 56 % of total variation
+pca = PCA(n_components=2) 
+principal_components = pca.fit_transform(scaled_features)
+print(f"The captured variance of total original variance in data:{np.sum(pca.explained_variance_ratio_)}%") # 58 % of total variance
 print(principal_components) # --> check
- 
-# Initialize KMeans with 5 clusters:
-kmeans = KMeans(n_clusters=5, random_state=42)
-# Fit the model, and indices of the cluster each sample belongs to:
-indices = kmeans.fit_predict(principal_components)
-print(indices)
- 
- 
+
+# Choosing the best n_clusters number
+range_n_clusters = [2, 3, 4, 5] 
+best_clusters = 0                  
+previous_silh = 0.0
+
+for n_clusters in range_n_clusters:
+    kmeans = KMeans(n_clusters=n_clusters, random_state=42)
+    cluster_labels = kmeans.fit_predict(principal_components)
+    sample_size = min(1000, len(principal_components))  # Reduce computation
+    silhouette = silhouette_score(principal_components[:sample_size], cluster_labels[:sample_size])
+    
+    if silhouette > previous_silh:
+        previous_silh = silhouette
+        best_clusters = n_clusters
+
+print(f"Best n_clusters is: {best_clusters}")
+
+# Initialize and fit KMeans with best n_clusters
+kmeans = KMeans(n_clusters=best_clusters, random_state=42)
+cluster_label = kmeans.fit_predict(principal_components)
+print(cluster_label)
+
 plt.figure(figsize=(15, 6))
- 
 # Scatter plot with color based on docking score 
 plt.subplot(1,2,1)
 norm=Normalize(vmin=-50, vmax=50)
 plt.scatter(principal_components[:, 0],principal_components[:, 1], c=df['score'], s=50, edgecolors='black', linewidths=0.5, cmap='plasma', norm=norm)
 plt.xlabel('Principal Component 1')
 plt.ylabel('Principal Component 2')
-plt.title('PCA of Drug Class Structures (Colored by Docking Score')
+plt.title('PCA of Drug Class Structures (Colored by Docking Score)')
 plt.colorbar(label='Docking Score')
  
 # Scatter plot with color based on clusters
 plt.subplot(1,2,2)
-plt.scatter(principal_components[:, 0], principal_components[:, 1], c=indices, s=50, edgecolors='black')
+cmap = plt.cm.viridis
+plt.scatter(principal_components[:, 0], principal_components[:, 1], c=cluster_label, cmap=cmap, s=50, edgecolors='black')
 plt.xlabel('Principal Component 1')
 plt.ylabel('Principal Component 2')
 plt.title('PCA of Drug Class Structures (Clustered by Kmeans)')
-legend_labels = list(set(indices))
-handles = [mpatches.Patch(color=plt.cm.viridis(i/4),label=legend_labels[i]) for i in range(len(legend_labels))]
-plt.legend(handles=handles)
+legend_labels = np.unique(cluster_label)
+# Normalize cluster labels to the range [0, 1] for the colormap
+norm = plt.Normalize(vmin=cluster_label.min(), vmax=cluster_label.max())
+# Create legend handles with the correct colors
+handles = [mpatches.Patch(color=cmap(norm(label)), label=f'Cluster {label}') for label in legend_labels]
+plt.legend(handles=handles, title="Clusters")
 plt.show()
  
 # Calculating the averages scores of docking scores per clusters:
 average_scores = []
 chemical_properties_per_cluster = {}
-# Loop throught (0-4):
-for index in list(set(indices)):
+# Loop throught (0-1):
+for index in list(set(cluster_label)):
     count = 0
-    sum = 0
+    total_score = 0
     chemical_properties_per_cluster[index] = []
     for i in range(len(df)):
-        if indices[i] == index:
-            sum += df.iloc[i]['score']
-            chemical_properties_per_cluster[index].append(numeric_columns.iloc[i])
+        if cluster_label[i] == index:
+            total_score += df.iloc[i]['score']
+            chemical_properties_per_cluster[index].append(features.iloc[i])
             count += 1
-    average_score = sum/count
+    average_score = total_score/count
     average_scores.append(average_score)
     print(f"Cluster {index} has average score:{average_score}.")
  
--- Stage-3/readme.md
+++ Stage-3/readme.md
@@ -1,38 +1,45 @@
-Import necessary libraries (`pandas, sklearn.decomposition import PCA, from sklearn.preprocessing import StandardScaler, matplotlib.pyplot, from sklearn.cluster import KMeans, numpy, from sklearn.metrics import silhouette_samples, silhouette_score, matplotlib.patches as mpatches, and matplotlib.colors import Normalize`)
+Import necessary libraries (`pandas`, `sklearn.decomposition import PCA`, `from sklearn.preprocessing import StandardScaler`, `matplotlib.pyplot`, `from sklearn.cluster import KMeans`, `from sklearn.metrics import silhouette_score`, `numpy`, `matplotlib.patches as mpatches`, and `matplotlib.colors import Normalize`)
 
+1) Create dataframe from imported data.
+2) Select only numeric columns that has chemical properties (`.drop` method is used).
+3) Scale the data with `StandardScaler()` to ensure that each feture contribute equally, before applying the PCA and clustering (Kmeans) algoritham.
+4) PCA, n_components=2 (2D) - the goal is to reduce the dimensionality of a dataset while preserving as much variance (information) as possible.
 **Printed output:** used for PCA graph:
 
 Principal component 1 (PC1) and Principal component 2 (PC2):
 ```text
-< [[  2.68793621  -0.72620336]
-< [  0.08183217   4.54497332]
-< [  5.5248472    3.85005726]
-< ...
-< [ -0.84416509 -11.29018516]
-< [ -4.99976647 -19.7985095 ]
-< [ -4.58655639 -30.48500731]]
+< [[ 2.51699613 -1.56144603]
+<  [-0.11059088  3.93586821]
+<  [ 5.35368569  3.36829477]
+<  ...
+<  [ 2.51128302  3.52608307]
+<  [-0.63269646 -0.85170953]
+<  [ 3.15243514  3.33707233]]
 ```
+ * To ensure that PCA is shows the enought varience between the selected features: `p.sum(pca.explained_variance_ratio_)` was used! It shows that (0.5787931695723) 58 % of total  variance of original features is captured by each principal component.
 
+5) Initializing the KMeans with chosen clusters. The best n_clusters number (`best_clusters`) is  chosen with `silhouette_score`.      So, the range of n_clusters were chosen [2,3,4,5] and the loop was created to go trought the range and compere the calculated silhouette with previously calculated silhouette.
+* The list `cluster_label` is created to store the information of clusters labels for each chemical compound in the original dataframe.
 **Printed output:**
 
 List for indices:
 ```text
-< [1 0 2 ... 1 1 1]
+< [1 0 1 ... 1 0 1]
 ```
+6) Plotting the Figure with two subplots, `plt.subplot(1,2,1)` is plot that shows PCA graph colored by the docking score (column: `score`), and `plt.sublot(1,2,2) is PCA graph colored by clusters.
 
 **Figure**
 ![image](https://github.com/user-attachments/assets/876bb046-73a2-4d65-8d59-1a839b967b0d)
 
+7) Creating the loop for calculating the average scores (`average_score`) of docking scores per clusters. First loop goes through unique set of `index` in indices list. Second loop goes through the all rows in original dataframe (df).                           Append all chemical properties information into the dictionary `chemical_properties_per_cluster = {}` where **key** is cluster number (`index` in loop), and **values** are chemical properties.
+
 **Printed output:**
 ```text
 Clusters average scores:
-< Cluster 0 has average score:-89.2632789380448.
-< Cluster 1 has average score:-62.771634127631216.
-< Cluster 2 has average score:-82.23764533779618.
-< Cluster 3 has average score:-57.42684203823773.
-< Cluster 4 has average score:-63.8510015343152.
+< Cluster 0 has average score:-68.15697424293974.
+< Cluster 1 has average score:-73.84337848531261.
 ```
 **Printed output:**
 ```text
-Cluster with lowest average docking score is: 0
+Cluster with lowest average docking score is: 1
 ```
